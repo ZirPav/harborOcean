339830ece152a5eeefbb094b12e738d7782d75ee
Pavel Ziryukin <zir-pav@mail.ru>
2020-12-27 16:30:41 +0300

    harborOcean v2

--- src/main/resources/static/demo2.js
+++ src/main/resources/static/demo2.js
@@ -1,15 +1,7 @@
 document.getElementById('ocean').addEventListener('click', go)
 
-/*function go() {
-    fetch('http://localhost:8088/demo2')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-        })
-}*/
-
 function go() {
-    fetch('http://localhost:8088/demo2')
+    fetch('https://quiet-peak-67801.herokuapp.com/demo2')
         .then(function (response) {
             console.log(response)
             if (response.ok) {
